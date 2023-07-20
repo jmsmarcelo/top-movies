7734a35474ff94d59198b554ce6bdde6eb1b4fd8
jmsmarcelo <32857346+jmsmarcelo@users.noreply.github.com>
2023-07-20 00:02:12 -0400

    Add Api class

--- src/com/github/jmsmarcelo/topmovies/head/Api.java
+++ src/com/github/jmsmarcelo/topmovies/head/Api.java
@@ -4,8 +4,5 @@ public class Api {
 	public static String url = "<Your API URL here>";
 	public static String auth = "<Your Authorization Key here>";
 	public static String urlImg = "<Your URL for image here>";
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 5fd3879c5e5fbd1ee6d90f58ed2cb89ad4d00d90
+
