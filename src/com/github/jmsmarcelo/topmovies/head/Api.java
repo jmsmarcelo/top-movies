5fd3879c5e5fbd1ee6d90f58ed2cb89ad4d00d90
Jose Marcelo <32857346+jmsmarcelo@users.noreply.github.com>
2023-07-19 23:40:35 -0400

    Add String for server image

--- src/com/github/jmsmarcelo/topmovies/head/Api.java
+++ src/com/github/jmsmarcelo/topmovies/head/Api.java
@@ -3,4 +3,5 @@ package com.github.jmsmarcelo.topmovies.head;
 public class Api {
 	public static String url = "<Your API URL here>";
 	public static String auth = "<Your Authorization Key here>";
+	public static String urlImg = "<Your URL for image here>";
 }
