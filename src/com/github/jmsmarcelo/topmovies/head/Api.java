b2fa14988e6c233e16b073d6472cbfcfcb3b37d7
Jose Marcelo <32857346+jmsmarcelo@users.noreply.github.com>
2023-07-19 23:48:00 -0400

    Delete Api.java

--- src/com/github/jmsmarcelo/topmovies/head/Api.java
+++
@@ -1,7 +0,0 @@
-package com.github.jmsmarcelo.topmovies.head;
-
-public class Api {
-	public static String url = "<Your API URL here>";
-	public static String auth = "<Your Authorization Key here>";
-	public static String urlImg = "<Your URL for image here>";
-}
