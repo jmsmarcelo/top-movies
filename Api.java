15053695adc33da32703cf9c1e9b131f4b7005d7
jmsmarcelo <32857346+jmsmarcelo@users.noreply.github.com>
2023-07-21 08:52:39 -0400

    Delete

--- Api.java
+++
@@ -1,8 +0,0 @@
-package com.github.jmsmarcelo.topmovies.head;
-
-public class Api {
-	public static String url = "https://api.themoviedb.org/3/movie/top_rated";
-	public static String auth = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNWRhOTZkYzVkMmMwZjhiNDZhOWFmOTVjYjYxYWE5NCIsInN1YiI6IjY0YjczZjI4ZjI2M2JhMDBlMjNiZmFlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RZ_GEeRzmgUR1xdANuPmobqMGKqcyUlZ2_sHlT6dcBI";
-	public static String urlImg = "https://image.tmdb.org/t/p/w300";
-}
-
