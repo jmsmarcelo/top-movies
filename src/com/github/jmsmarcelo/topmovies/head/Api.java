1c2fde606795ef9b47d8340dad34674be918194d
jmsmarcelo <32857346+jmsmarcelo@users.noreply.github.com>
2023-07-20 23:23:04 -0400

    Update api

---
+++ src/com/github/jmsmarcelo/topmovies/head/Api.java
@@ -0,0 +1,8 @@
+package com.github.jmsmarcelo.topmovies.head;
+
+public class Api {
+	public static String url = "<Your URL API here>";
+	public static String auth = "<Your Authorization Key here>";
+	public static String urlImg = "<Your URL image here>";
+}
+
