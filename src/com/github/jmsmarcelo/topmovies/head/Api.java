ac70fe7138d2910139978c05cd2eef69f3f9515c
jmsmarcelo <32857346+jmsmarcelo@users.noreply.github.com>
2023-07-20 23:04:53 -0400

    Update api

--- src/com/github/jmsmarcelo/topmovies/head/Api.java
+++ src/com/github/jmsmarcelo/topmovies/head/Api.java
@@ -1,8 +1,8 @@
 package com.github.jmsmarcelo.topmovies.head;
 
 public class Api {
-	public static String url = "https://api.themoviedb.org/3/movie/top_rated";
-	public static String auth = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNWRhOTZkYzVkMmMwZjhiNDZhOWFmOTVjYjYxYWE5NCIsInN1YiI6IjY0YjczZjI4ZjI2M2JhMDBlMjNiZmFlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RZ_GEeRzmgUR1xdANuPmobqMGKqcyUlZ2_sHlT6dcBI";
-	public static String urlImg = "https://image.tmdb.org/t/p/w300";
+	public static String url = "<Your Url API here>";
+	public static String auth = "<Your Authorization Key here>";
+	public static String urlImg = "<Your url image here>";
 }
 
